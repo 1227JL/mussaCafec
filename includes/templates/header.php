b6f875fc3ea5821e43030ec989248f985756ad74
Cardenas2915 <eduar.cardenas15@gmail.com>
2023-08-30 23:59:11 -0500

    Se realizaron cambios en el footer y tamaño de letras en el form

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mussa Cafec</title>
     <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="shortcut icon" href="/build/assets/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
