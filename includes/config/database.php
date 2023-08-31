15f49c02d6493cc4d8142b3df810445fcc0961b2
Cardenas2915 <eduar.cardenas15@gmail.com>
2023-08-30 22:52:21 -0500

    se agrego el archivo de feriaempresarial

--- includes/config/database.php
+++ includes/config/database.php
@@ -26,3 +26,13 @@ function conectarDB(): mysqli {
     return $db;
 }
 
+// function conectarDB(): mysqli {
+//     $db = new mysqli('15.235.85.112', 'mussacaf_root', 'Mussacafec2023*', 'mussacaf_mussacafec');
+
+//     if(!$db){
+//         echo 'Error, no se pudo conectar';
+//         exit;
+//     }
+
+//     return $db;
+// }
